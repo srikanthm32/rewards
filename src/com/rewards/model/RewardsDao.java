065ec83f53c26d2e8b5fa26d8effe8adef861361
srikanthm32 <70723139+srikanthm32@users.noreply.github.com>
2020-09-09 12:54:51 -0400

    Update RewardsDao.java

--- src/com/rewards/model/RewardsDao.java
+++ src/com/rewards/model/RewardsDao.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import com.rewards.dto.CustomerRewardsDTO;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
 	@Query("SELECT new com.rewards.dto(e.name, e.name, e.email, e.address) "
 			+ "FROM Order O OUTER JOIN Customers e where e.order_id = o.order_id where c.customer_id = "+ customerId +" and orderDate >= "+orderDate)
-	List<DeptEmpDto> fetchCustomerOrderDeptDataLeftJoin(String customerId, String orderDate);
+	List<CustomerRewardsDTO> fetchCustomerOrderDeptDataLeftJoin(String customerId, String orderDate);
 
 }
